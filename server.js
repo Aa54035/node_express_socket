4efe2f075de10ce4b615156404f406d69fb6eb3d
ashish <aa54035@yahoo.com>
2020-06-04 21:44:54 +0530

    successfull mongdb save

--- server.js
+++ server.js
@@ -3,11 +3,31 @@ var bodyParser = require('body-parser')
 var app = express()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
-var port = 3300;
+var mongoose  = require('mongoose')
+var port = 3000;
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+ 
+var DBURL =
+ 'mongodb+srv://admin:admin@mongoclustor-t0m10.mongodb.net/node_socket_Mongo?retryWrites=true&w=majority';
+
+// var DBURL =
+//   'mongodb+srv://user:user123@mongoclustor-t0m10.mongodb.net/node_socket_Mongo?retryWrites=true&w=majority';
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user:user@mongoclustor-t0m10.mongodb.net/node_socket_Mongo.node_socket_Mongo?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//      console.log('DB connection ==>'+err);
+// });
+   
+var Message = mongoose.model('Message',{
+    name : String ,
+    message : String 
+})
 
 var messages = [
     {name: 'Tim', message: 'Hi'},
@@ -19,16 +39,28 @@ app.get('/messages', (req, res) =>{
 })
 
 app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
-})
+ var message = new Message(req.body);
+ message.save((Err)=>{
+     if (Err) {
+         res.sendStatus(5000);
+     }
+     messages.push(req.body)
+     io.emit('message', req.body)
+     res.sendStatus(200)
+ 
+ })
+    })
+//This post pushes input to Arraya and Emit message 
 
+mongoose.connect(DBURL,(err)=>{
+    console.log('DB connection ==>'+err);
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+    console.log('a user connected')
 })
-
+ 
 server.listen(port, () => {
     console.log('server is listening on port', port)
 })
